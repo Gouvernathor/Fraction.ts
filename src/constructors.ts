8e7f0cd479c831d36376fd05e90cd5b7b2c4a46b
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-07-19 01:02:50 +0200

    Stub constructors

---
+++ src/constructors.ts
@@ -0,0 +1,22 @@
+import { Fraction, FractionAble } from "./interface";
+
+export declare function fromNumber(num: number): Fraction;
+export declare function fromBigInt(num: bigint): Fraction;
+export declare function fromString(str: string): Fraction; // re-export a parse function
+export declare function fromTuple([num, denom]: [bigint|number, bigint|number]): Fraction;
+export declare function fromObject(obj: { numerator: bigint; denominator: bigint }): Fraction;
+
+export function fromAny(obj: FractionAble): Fraction {
+    if (typeof obj === "number") {
+        return fromNumber(obj);
+    } else if (typeof obj === "bigint") {
+        return fromBigInt(obj);
+    } else if (typeof obj === "string") {
+        return fromString(obj);
+    } else if (Array.isArray(obj)) {
+        return fromTuple(obj as [bigint|number, bigint|number]);
+    } else if (typeof obj === "object" && obj !== null) {
+        return fromObject(obj as { numerator: bigint; denominator: bigint });
+    }
+    throw new TypeError(`Unsupported type for Fraction creation for ${obj}`);
+}
