96be0afd1c5fbd1010becbbad40dd632fe2f72de
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-07-20 02:53:20 +0200

    Fix fromNumber

--- src/fromNumber.ts
+++ src/fromNumber.ts
@@ -1,16 +1,24 @@
+/**
+ * FIXME This function doesn't seem to return the most exact fraction for a float (64).
+ */
 export function tupleFromNumber(num: number): [bigint, bigint] {
-    let z = 1;
-    let n = 0;
-    let d = 1;
-
+    let s = 1;
     if (num < 0) {
+        s = -1;
+        num = -num;
+    }
+
+    let z = 1;
+
+    if (num >= 1) {
         z = 10 ** Math.floor(1 + Math.log10(num));
         num /= z;
     }
 
     let A = 0, B = 1, C = 1, D = 0;
-
-    let N = 10_000_000;
+    let n = 0;
+    let d = 1;
+    const N = 10_000_000;
 
     while (B <= N && D <= N) {
         const M = (A + C) / (B + D);
@@ -45,5 +53,5 @@ export function tupleFromNumber(num: number): [bigint, bigint] {
             }
         }
     }
-    return [BigInt(n * z), BigInt(d)];
+    return [BigInt(n * z * s), BigInt(d)];
 }
