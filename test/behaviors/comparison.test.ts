4d045323eb12c7e16f59ea7cdadea0c3c32e3604
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-04-10 01:32:08 +0200

    Add tests for infinity value comparison

--- test/behaviors/comparison.test.ts
+++ test/behaviors/comparison.test.ts
@@ -36,3 +36,47 @@ it("should compare different fractions correctly", () => {
     expect(b.gt(a)).toBe(true);
     expect(b.gte(a)).toBe(true);
 });
+
+it("should compare infinities correctly", () => {
+    expect(Fraction.compare(Infinity, Infinity)).toBe(0);
+    expect(Fraction.compare(Infinity, -Infinity)).toBe(1);
+    expect(Fraction.compare(-Infinity, Infinity)).toBe(-1);
+    expect(Fraction.compare(-Infinity, -Infinity)).toBe(0);
+});
+
+it("should compare with infinities correctly", () => {
+    const pos = Fraction(54, 23);
+    const neg = Fraction(-43, 27);
+
+    expect(pos.compareTo(Infinity)).toBe(-1);
+    expect(Fraction.compare(pos, Infinity)).toBe(-1);
+    expect(pos.equals(Infinity)).toBe(false);
+    expect(pos.lt(Infinity)).toBe(true);
+    expect(pos.lte(Infinity)).toBe(true);
+    expect(pos.gt(Infinity)).toBe(false);
+    expect(pos.gte(Infinity)).toBe(false);
+
+    expect(neg.compareTo(Infinity)).toBe(-1);
+    expect(Fraction.compare(neg, Infinity)).toBe(-1);
+    expect(neg.equals(Infinity)).toBe(false);
+    expect(neg.lt(Infinity)).toBe(true);
+    expect(neg.lte(Infinity)).toBe(true);
+    expect(neg.gt(Infinity)).toBe(false);
+    expect(neg.gte(Infinity)).toBe(false);
+
+    expect(pos.compareTo(-Infinity)).toBe(1);
+    expect(Fraction.compare(pos, -Infinity)).toBe(1);
+    expect(pos.equals(-Infinity)).toBe(false);
+    expect(pos.lt(-Infinity)).toBe(false);
+    expect(pos.lte(-Infinity)).toBe(false);
+    expect(pos.gt(-Infinity)).toBe(true);
+    expect(pos.gte(-Infinity)).toBe(true);
+
+    expect(neg.compareTo(-Infinity)).toBe(1);
+    expect(Fraction.compare(neg, -Infinity)).toBe(1);
+    expect(neg.equals(-Infinity)).toBe(false);
+    expect(neg.lt(-Infinity)).toBe(false);
+    expect(neg.lte(-Infinity)).toBe(false);
+    expect(neg.gt(-Infinity)).toBe(true);
+    expect(neg.gte(-Infinity)).toBe(true);
+});
